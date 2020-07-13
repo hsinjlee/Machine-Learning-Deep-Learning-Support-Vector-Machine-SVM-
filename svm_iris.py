5cf08075d990291dd71144fd843af89c1bb3906f
hsinjlee <hlee294@asu.edu>
2020-07-12 17:28:20 -0700

    add svm digits and svm iris

---
+++ svm_iris.py
@@ -0,0 +1,42 @@
+from sklearn import svm
+# from sklearn.cross_validation import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+#
+#	Important parameters for SVC: gamma and C
+#		gamma -> defines how far the influence of a single training example reaches
+#					Low value: influence reaches far      High value: influence reaches close
+#
+#		C -> trades off hyperplane surface simplicity + training examples missclassifications
+#					Low value: simple/smooth hyperplane surface 
+#					High value: all training examples classified correctly but complex surface
+
+
+dataset = datasets.load_iris()
+
+# print(dataset)
+
+features = dataset.data
+targetVariables = dataset.target
+
+featureTrain, featureTest, targetTrain, targetTest = train_test_split(features, targetVariables, test_size=0.3)
+
+# model = svm.SVC(gamma=0.001, C=100)
+model = svm.SVC()
+fittedModel = model.fit(featureTrain, targetTrain)
+predictions = fittedModel.predict(featureTest)
+
+print("confusion_matrix(targetTest, predictions)")
+print(confusion_matrix(targetTest, predictions))
+print("accuracy_score(targetTest, predictions)")
+print(accuracy_score(targetTest, predictions))
+
+# confusion_matrix(targetTest, predictions)
+# [[17  0  0]
+#  [ 0 14  0]
+#  [ 0  1 13]]
+# accuracy_score(targetTest, predictions)
+# 0.9777777777777777
